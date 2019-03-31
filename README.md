a9ca376dbf6b098b7f15e19f26c596b0ad505982
Pawel <jarrodek@gmail.com>
2019-03-30 18:10:59 -0700

    Breaking: Upgrading to Polymer 3

--- README.md
+++ README.md
@@ -1,24 +1,76 @@
+[![Published on NPM](https://img.shields.io/npm/v/@advanced-rest-client/cookie-details.svg)](https://www.npmjs.com/package/@advanced-rest-client/cookie-details)
+
 [![Build Status](https://travis-ci.org/advanced-rest-client/cookie-details.svg?branch=stage)](https://travis-ci.org/advanced-rest-client/cookie-details)
 
-## UiElements.CookieDetails component
-Tag: `<cookie-details>`
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/advanced-rest-client/cookie-details)
+
+An element to render Coockie object details.
+
+### Example
+
+```html
+<cookie-details></cookie-details>
+```
+
+### API components
+
+This components is a part of [API components ecosystem](https://elements.advancedrestclient.com/)
+
+## Usage
 
 ### Installation
-Using bower:
 ```
-bower install --save advanced-rest-client/cookie-details
+npm install --save @advanced-rest-client/cookie-details
 ```
 
-A cookie details view.
+### In an html file
 
-## API
-### Component properties (attributes)
+```html
+<html>
+  <head>
+    <script type="module">
+      import './node_odules/@advanced-rest-client/cookie-details/cookie-details.js';
+    </script>
+  </head>
+  <body>
+    <cookie-details></cookie-details>
+  </body>
+</html>
+```
 
-#### cookie
-- Type: `Object`
-A cookie to render
+### In a Polymer 3 element
 
+```js
+import {PolymerElement, html} from './node_odules/@polymer/polymer';
+import './node_odules/@advanced-rest-client/cookie-details/cookie-details.js';
 
-### Component methods
+class SampleElement extends PolymerElement {
+  static get template() {
+    return html`
+    <cookie-details></cookie-details>
+    `;
+  }
+}
+customElements.define('sample-element', SampleElement);
+```
 
+### Installation
 
+```sh
+git clone https://github.com/advanced-rest-client/cookie-details
+cd api-url-editor
+npm install
+npm install -g polymer-cli
+```
+
+### Running the demo locally
+
+```sh
+polymer serve --npm
+open http://127.0.0.1:<port>/demo/
+```
+
+### Running the tests
+```sh
+polymer test --npm
+```
--- cookie-details.js
+++ cookie-details.js
@@ -11,16 +11,14 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import { PolymerElement } from '../../@polymer/polymer/polymer-element.js';
-
-import '../../date-time/date-time.js';
-import '../../arc-icons/arc-icons.js';
+import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
+import {IronResizableBehavior} from '../../@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
+import {html} from '../../@polymer/polymer/lib/utils/html-tag.js';
+import {mixinBehaviors} from '../../@polymer/polymer/lib/legacy/class.js';
+import '../../@advanced-rest-client/date-time/date-time.js';
+import '../../@advanced-rest-client/arc-icons/arc-icons.js';
 import '../../@polymer/paper-button/paper-button.js';
 import '../../@polymer/iron-icon/iron-icon.js';
-import '../../@polymer/iron-flex-layout/iron-flex-layout.js';
-import { IronResizableBehavior } from '../../@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
-import { html } from '../../@polymer/polymer/lib/utils/html-tag.js';
-import { mixinBehaviors } from '../../@polymer/polymer/lib/legacy/class.js';
 /**
  * A cookie details view.
  *
@@ -28,7 +26,7 @@ import { mixinBehaviors } from '../../@polymer/polymer/lib/legacy/class.js';
  * @polymer
  * @demo demo/index.html
  * @memberof UiElements
- * @appliesMixin Polymer.IronResizableBehavior
+ * @polymerBehavior IronResizableBehavior
  */
 class CookieDetails extends mixinBehaviors(IronResizableBehavior, PolymerElement) {
   static get template() {
@@ -36,88 +34,107 @@ class CookieDetails extends mixinBehaviors(IronResizableBehavior, PolymerElement
     <style>
     :host {
       display: block;
-      @apply --arc-font-body1;
-      @apply --cookie-details;
+      font-size: var(--arc-font-body1-font-size);
+      font-weight: var(--arc-font-body1-font-weight);
+      line-height: var(--arc-font-body1-line-height);
     }
 
     h2 {
-      @apply --arc-font-headline;
+      font-size: var(--arc-font-headline-font-size);
+      font-weight: var(--arc-font-headline-font-weight);
+      letter-spacing: var(--arc-font-headline-letter-spacing);
+      line-height: var(--arc-font-headline-line-height);
+    }
+
+    .lh {
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -ms-flex-direction: row;
+      -webkit-flex-direction: row;
+      flex-direction: row;
     }
 
     .meta-row {
-      @apply --layout-horizontal;
-      @apply --layout-center;
-      color: rgba(0, 0, 0, 0.87);
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      align-items: center;
+      color: var(--cookie-details-meta-color, rgba(0, 0, 0, 0.87));
       height: 40px;
-      @apply --saved-request-detail-meta-row;
     }
 
     .meta-row .label {
       width: 160px;
       display: inline-block;
-      @apply --saved-request-detail-meta-row-label;
     }
 
     .meta-row .value {
       display: inline-block;
-      @apply --arc-font-common-nowrap;
-      @apply --saved-request-detail-meta-row-value;
+      white-space: var(--arc-font-nowrap-white-space);
+      overflow: var(--arc-font-nowrap-overflow);
+      text-overflow: var(--arc-font-nowrap-text-overflow);
     }
 
     .actions {
-      @apply --layout-horizontal;
-      @apply --layout-end-justified;
+      -ms-flex-pack: end;
+      -webkit-justify-content: flex-end;
+      justify-content: flex-end;
       margin-top: 20px;
-      @apply --saved-request-detail-actions-container;
     }
 
     .actions paper-button {
       color: var(--primary-color);
       padding-left: 12px;
       padding-right: 12px;
-      @apply --saved-request-detail-action-buttons;
     }
 
     paper-button iron-icon {
       margin-right: 12px;
       color: var(--saved-request-detail-action-icon-color, rgba(0, 0, 0, 0.54));
-      @apply --saved-request-detail-action-icon;
     }
     </style>
     <h2>[[cookie.name]]</h2>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Value</span>
       <span class="value">[[cookie.value]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Domain</span>
       <span class="value">[[cookie.domain]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Path</span>
       <span class="value">[[cookie.path]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Expires</span>
-      <span class="value"><date-time date="[[cookie.expires]]" year="numeric" month="numeric" day="numeric" hour="numeric" minute="numeric"></date-time></span>
+      <span class="value">
+        <date-time
+          date="[[cookie.expires]]"
+          year="numeric"
+          month="numeric"
+          day="numeric"
+          hour="numeric"
+          minute="numeric"></date-time>
+      </span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Host only</span>
       <span class="value">[[cookie.hostOnly]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">HTTP only</span>
       <span class="value">[[cookie.httpOnly]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Secure</span>
       <span class="value">[[cookie.secure]]</span>
     </div>
-    <div class="meta-row">
+    <div class="meta-row lh">
       <span class="label">Session</span>
       <span class="value">[[cookie.session]]</span>
     </div>
-    <div class="actions">
+    <div class="actions lh">
       <paper-button on-tap="_deleteCookie" data-action="delete-action">
         <iron-icon icon="arc:delete"></iron-icon>
         Delete
@@ -130,7 +147,6 @@ class CookieDetails extends mixinBehaviors(IronResizableBehavior, PolymerElement
 `;
   }
 
-  static get is() { return 'cookie-details'; }
   static get properties() {
     return {
       // A cookie to render
@@ -185,4 +201,4 @@ class CookieDetails extends mixinBehaviors(IronResizableBehavior, PolymerElement
    * @param {Object} cookie The cookie object
    */
 }
-window.customElements.define(CookieDetails.is, CookieDetails);
+window.customElements.define('cookie-details', CookieDetails);
